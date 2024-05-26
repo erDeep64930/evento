965d2a3f329c92af4f73e9d7c79eb43befe38e1f
erDeep640390 <erdeep64930@gmail.com>
2024-05-26 10:59:42 +0530

    db.ts file has been added

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,7 +1,8 @@
 
 import { twMerge } from "tailwind-merge";
 import clsx, { ClassValue } from "clsx";
-import { EventoEvent, PrismaClient ,Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
 
 const prisma = new PrismaClient();
 
@@ -18,24 +19,22 @@ export async function sleep(ms: number) {
 export function capitalize(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-export async function getEvents(city:string) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
+export async function getEvents(city: string) {
 
-  const events: EventoEvent[] = await response.json();
+  const events = await prisma.eventoEvent.findMany({
+    where: {
+      city:city==="all"?undefined: capitalize(city),
+    },
+  });
   return events;
 }
 
-export async function getEvent(slug:string){
-  // const response = await fetch(
-  //   `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  // );
-  // const event:EventoEvent = await response.json();
- const event = await prisma.eventoEvent.findUnique({
-    where:{
-      slug:slug,
-    }
-  })
+export async function getEvent(slug: string) {
+
+  const event = await prisma.eventoEvent.findUnique({
+    where: {
+      slug: slug,
+    },
+  });
   return event;
 }
